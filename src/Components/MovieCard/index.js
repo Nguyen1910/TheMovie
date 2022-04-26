a1b1a35118b462a6d8780f3e9cadc89589bbbb86
Nguyen1910 <79495141+Nguyen1910@users.noreply.github.com>
2022-04-26 16:01:01 +0700

    first commit

--- src/Components/MovieCard/index.js
+++ src/Components/MovieCard/index.js
@@ -6,7 +6,7 @@ const index = ({ data, category, className }) => {
   return (
     <div>
       <div className={`movie__card ${className}`}>
-        <a href={`/${category}/detail/${data.id}`}>
+        <Link to={`/${category}/detail/${data.id}`}>
           <img
             src={apiConfig.w500lImagePath(data.poster_path)}
             alt="data.original_title"
@@ -14,7 +14,7 @@ const index = ({ data, category, className }) => {
           <h3 className="movie_title">
             {data.original_title || data.original_name}
           </h3>
-        </a>
+        </Link>
       </div>
     </div>
   );
